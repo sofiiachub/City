da59867943a651792755042c5297e9f15df5b9fd
sofiiachub <chub@stu.cn.ua>
2025-04-12 12:19:17 +0200

    first commit

---
+++ src/main/java/com/city/goodcity/GoodcityApplication.java
@@ -0,0 +1,13 @@
+package com.city.goodcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodcityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodcityApplication.class, args);
+	}
+
+}
